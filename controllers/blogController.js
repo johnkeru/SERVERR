c215f2a8b87ad2974c0a801d9ed52b131e1045d6
john keru <keruk355@gmail.com>
2024-09-25 15:21:17 +0800

    done blog crud

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -9,12 +9,14 @@ exports.getAllBlogs = async (req, res) => {
     }
 }
 
-exports.getBlog = async (req, res) => {
+exports.getBlogById = async (req, res) => {
     try {
         const id = req.params.id
         const blog = await Blog.findById(id)
         if (!blog) return res.status(404).json({ message: 'Blog not found' })
-        res.json(blog)
+        res.json({ blog })
+        // api.get('/blog')
+        // .then(res => res.data.blog)
     } catch (e) {
         res.status(500).json({ error: 'Something went wrong' })
     }
@@ -22,7 +24,10 @@ exports.getBlog = async (req, res) => {
 
 exports.createBlog = async (req, res) => {
     try {
-
+        const body = req.body
+        const blog = new Blog(body)
+        const savedBlog = await blog.save()
+        res.status(201).json(savedBlog)
     } catch (e) {
         res.status(500).json({ error: 'Something went wrong' })
     }
@@ -30,7 +35,12 @@ exports.createBlog = async (req, res) => {
 
 exports.updateBlog = async (req, res) => {
     try {
-
+        const id = req.params.id
+        const body = req.body
+        const isBlogExists = await Blog.findById(id)
+        if (!isBlogExists) return res.status(404).json({ error: 'Blog not found' })
+        const updatedBlog = await Blog.findByIdAndUpdate(id, body, { new: true })
+        res.json({ blog: updatedBlog })
     } catch (e) {
         res.status(500).json({ error: 'Something went wrong' })
     }
@@ -38,7 +48,8 @@ exports.updateBlog = async (req, res) => {
 
 exports.deleteBlog = async (req, res) => {
     try {
-
+        const deletedBlog = await Blog.findOneAndDelete(req.params.id)
+        res.json({ blog: deletedBlog })
     } catch (e) {
         res.status(500).json({ error: 'Something went wrong' })
     }
--- routers/blogRouter.js
+++ routers/blogRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const { getAllBlogs, getBlog, createBlog, updateBlog, deleteBlog } = require('../controllers/blogController');
+const { getAllBlogs, getBlogById, createBlog, updateBlog, deleteBlog } = require('../controllers/blogController');
 
 const blogRouter = Router();
 
 blogRouter.get('/blogs', getAllBlogs);
-blogRouter.get('/blogs/:id', getBlog);
+blogRouter.get('/blogs/:id', getBlogById);
 blogRouter.post('/blogs', createBlog);
 blogRouter.put('/blogs/:id', updateBlog);
 blogRouter.delete('/blogs/:id', deleteBlog);
